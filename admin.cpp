5a83b9ee79e9a3aff29c4c1015949106f49412d4
I am <99683656+discriminating@users.noreply.github.com>
2023-03-19 00:10:44 +0000

    Fix ugly code in admin.cpp

--- admin.cpp
+++ admin.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <Windows.h>
 
-bool isExecutableInPath(const char* exeName)
-{
+bool isExecutableInPath(const char* exeName) {
 	// Get the PATH environment variable
 	char pathVar[MAX_PATH];
-	if (!GetEnvironmentVariableA("PATH", pathVar, MAX_PATH))
-	{
+	if (!GetEnvironmentVariableA("PATH", pathVar, MAX_PATH)) {
 		std::cout << "Failed to get PATH environment variable" << std::endl;
 		return false;
 	}
@@ -14,34 +12,25 @@ bool isExecutableInPath(const char* exeName)
 	// Split the PATH into individual directories
 	char* context;
 	char* dir = strtok_s(pathVar, ";", &context);
-	while (dir)
-	{
+	while (dir) {
 		// Check if the executable file exists in the directory
 		std::string fullPath = std::string(dir) + "\\" + exeName;
-		if (GetFileAttributesA(fullPath.c_str()) != INVALID_FILE_ATTRIBUTES)
-		{
-			return true;
-		}
+		if (GetFileAttributesA(fullPath.c_str()) != INVALID_FILE_ATTRIBUTES) return true;
 
 		// Move on to the next directory in the PATH
 		dir = strtok_s(NULL, ";", &context);
-	}
-
-	return false;
+	} return false;
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
 	// Check if the executable is in the PATH
 	const char* exeName = strrchr(argv[0], '\\') ? strrchr(argv[0], '\\') + 1 : argv[0];
-	if (!isExecutableInPath(exeName))
-	{
+	if (!isExecutableInPath(exeName)) {
 		// Prompt the user to add the executable to the PATH
 		std::cout << exeName << " is not in your PATH environment variable. This can cause errors and annoyance upon using." << std::endl;
 	}
 
-	if (argc < 2)
-	{
+	if (argc < 2) {
 		std::cout << "Usage: " << argv[0] << " <exe_path> [args...]" << std::endl;
 		return 1;
 	}
@@ -49,10 +38,7 @@ int main(int argc, char* argv[])
 	// Get the command-line arguments
 	const char* exePath = argv[1];
 	std::string args;
-	for (int i = 2; i < argc; i++)
-	{
-		args += std::string(argv[i]) + " ";
-	}
+	for (int i = 2; i < argc; i++) { args += std::string(argv[i]) + " "; }
 
 	// Start the process with admin privileges
 	SHELLEXECUTEINFOA shellInfo = { sizeof(shellInfo) };
@@ -60,11 +46,8 @@ int main(int argc, char* argv[])
 	shellInfo.lpFile = exePath;
 	shellInfo.lpParameters = args.c_str(); // Pass additional arguments
 	shellInfo.nShow = SW_SHOW;
-	if (!ShellExecuteExA(&shellInfo))
-	{
+	if (!ShellExecuteExA(&shellInfo)) {
 		std::cout << "Failed to run process with admin privileges" << std::endl;
 		return 2;
-	}
-
-	return 0;
+	} return 0;
 }
